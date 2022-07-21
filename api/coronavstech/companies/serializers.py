f9de3f3c06768c0171e5b9d3998d6a8d55ff5ad1
Fernando Perardt <fperardt@gmail.com>
2022-07-21 13:09:53 -0300

    created companies rest

---
+++ api/coronavstech/companies/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Company
+
+class CompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Company
+        fields = ['id', 'name', 'status', 'last_update', 'application_link', 'note']
