eea97e85d0dde75877a9697a2fa8fed34225655d
Fernando Perardt <fperardt@gmail.com>
2022-07-22 14:41:41 -0300

    Changed test_api to use pytest native

--- api/coronavstech/companies/test/test_api.py
+++ api/coronavstech/companies/test/test_api.py
@@ -1,74 +1,62 @@
 import json
 import pytest
-from unittest import TestCase
-from django.test import Client
 from django.urls import reverse
 from companies.models import Company
 
-@pytest.mark.django_db
-class BasicCompanyApiTestCase(TestCase):
-    def setUp(self):
-        self.client = Client()
-        self.companies_url = reverse('companies-list')
-        
-        
-    def tearDown(self) -> None:
-        pass
+companies_url = reverse('companies-list')
+pytestmark = pytest.mark.django_db
 
+## GET
+def test_zero_companies_should_return_empty_list(client) -> None:     
+    response = client.get(companies_url)
+    assert response.status_code == 200
+    assert json.loads(response.content) == []
 
-class TestGetCompanies(BasicCompanyApiTestCase):
+def test_one_compan_exists_should_succeed(client) -> None:
+    amazon = Company.objects.create(name='Amazon')        
+    response = client.get(companies_url)
+    assert response.status_code == 200
+    assert json.loads(response.content)[0].get('name') == 'Amazon'
+    assert json.loads(response.content)[0].get('status') == 'Hiring'
+## POST
+
+def test_create_company_without_argument_should_fail(client):
+    response = client.post(path=companies_url)
+    assert response.status_code == 400
+    assert json.loads(response.content) == {'name': ['This field is required.']}
     
-    def test_zero_companies_should_return_empty_list(self) -> None:     
-        response = self.client.get(self.companies_url)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(json.loads(response.content), [])
-        
-    def test_one_compan_exists_should_succeed(self) -> None:
-        amazon = Company.objects.create(name='Amazon')        
-        response = self.client.get(self.companies_url)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(json.loads(response.content)[0].get('name'), 'Amazon')
-        self.assertEqual(json.loads(response.content)[0].get('status'), 'Hiring')
-        
-        amazon.delete()
-        
-class TestPostCompanies(BasicCompanyApiTestCase):
-    def test_create_company_without_argument_should_fail(self):
-        response = self.client.post(path=self.companies_url)
-        self.assertEqual(response.status_code, 400)
-        self.assertEqual(json.loads(response.content), {'name': ['This field is required.']})
-        
-        
-    def test_create_existing_company_should_fail(self):
-        Company.objects.create(name='Amazon')
-        response = self.client.post(path=self.companies_url, data={'name': 'Amazon'})
-        self.assertEqual(response.status_code, 400)
-        self.assertEqual(json.loads(response.content), {'name': ['company with this name already exists.']})
-        
-        
-    def test_create_company_with_only_name_all_fields_should_br_default(self):
-        response = self.client.post(path=self.companies_url, data={'name': 'Amazon'})
-        self.assertEqual(response.status_code, 201)
-        self.assertEqual(json.loads(response.content).get('name'), 'Amazon')
-        self.assertEqual(json.loads(response.content).get('status'), 'Hiring')
-        
-        
-    def test_create_company_with_layoffs_status_shuould_succeed(self):
-        response = self.client.post(path=self.companies_url, data={'name': 'Amazon', 'status': 'Layoffs'})
-        self.assertEqual(response.status_code, 201)
-        self.assertEqual(json.loads(response.content).get('name'), 'Amazon')
-        self.assertEqual(json.loads(response.content).get('status'), 'Layoffs')
-        
-    def test_create_company_with_wrong_status_shuould_fail(self):
-        response = self.client.post(path=self.companies_url, data={'name': 'Amazon', 'status': 'WrongStatus'})
-        self.assertEqual(response.status_code, 400)
-        self.assertIn('WrongStatus', str(response.content))
-        self.assertIn('is not a valid choice', str(response.content))
 
-    @pytest.mark.xfail
-    def test_shold_be_ok_if_fails(self):
-        self.assertEqual(1, 2)
-        
-    @pytest.mark.skip
-    def test_shold_be_skipped(self):
-        self.assertEqual(1, 2)
+def test_create_existing_company_should_fail(client):
+    Company.objects.create(name='Amazon')
+    response = client.post(path=companies_url, data={'name': 'Amazon'})
+    assert response.status_code == 400
+    assert json.loads(response.content) == {'name': ['company with this name already exists.']}
+    
+
+def test_create_company_with_only_name_all_fields_should_br_default(client):
+    response = client.post(path=companies_url, data={'name': 'Amazon'})
+    assert response.status_code == 201
+    assert json.loads(response.content).get('name') == 'Amazon'
+    assert json.loads(response.content).get('status') == 'Hiring'
+    
+
+def test_create_company_with_layoffs_status_shuould_succeed(client):
+    response = client.post(path=companies_url, data={'name': 'Amazon', 'status': 'Layoffs'})
+    assert response.status_code == 201
+    assert json.loads(response.content).get('name') == 'Amazon'
+    assert json.loads(response.content).get('status') == 'Layoffs'
+
+
+def test_create_company_with_wrong_status_shuould_fail(client):
+    response = client.post(path=companies_url, data={'name': 'Amazon', 'status': 'WrongStatus'})
+    assert response.status_code == 400
+    assert 'WrongStatus' in str(response.content)
+    assert 'is not a valid choice' in str(response.content)
+
+@pytest.mark.xfail
+def test_shold_be_ok_if_fails():
+    assert 1 == 2
+    
+@pytest.mark.skip
+def test_shold_be_skipped():
+    assert 1 == 2
