1a5b2f74b40f2c9c8ce055362961292c77cb3f3d
Fernando Perardt <fperardt@gmail.com>
2022-07-26 09:43:11 -0300

    added cached fibonacci solution

--- fibonacci/tests/test_fibonacci.py
+++ fibonacci/tests/test_fibonacci.py
@@ -1,7 +1,9 @@
 import pytest
 from fibonacci.naive import fibonacci_naive
+from fibonacci.cached import fibonacci_cached, fibonacci_lru_cached
 
+@pytest.mark.parametrize('fib_func', [fibonacci_naive, fibonacci_cached, fibonacci_lru_cached])
 @pytest.mark.parametrize('n, expected', [(0, 0), (1, 1), (2, 1), (20, 6765)])
-def test_naive(n, expected):
-    res = fibonacci_naive(n)
+def test_fibonacci(fib_func, n, expected):
+    res = fib_func(n)
     assert res == expected
